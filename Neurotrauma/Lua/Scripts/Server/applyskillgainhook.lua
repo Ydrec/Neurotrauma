728f09eccf2d5515395a818420a13cf4d0020d0e
OlegBSTU <polybox45@gmail.com>
2025-01-18 02:29:12 +0500

    Revert remove medical crafting skill, dont need it

--- Neurotrauma/Lua/Scripts/Server/applyskillgainhook.lua
+++
@@ -1,5 +0,0 @@
-Hook.Patch("Barotrauma.CharacterInfo", "ApplySkillGain", function(instance, ptable)
-	if ptable["skillIdentifier"].ToString() == "medical" and not ptable["gainedFromAbility"] then
-		ptable.PreventExecution = true
-	end
-end, Hook.HookMethodType.Before)
--- Neurotrauma/Lua/todo.md
+++ Neurotrauma/Lua/todo.md
@@ -1,3 +1,4 @@
 # TODO:
 - implement body temp
+- rebalance skill gain amount from existing treatments
 - rest are probably in .lua files themselves
