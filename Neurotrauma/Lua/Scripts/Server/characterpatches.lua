e82c5b5650c2d6debf0cd5a22bcee5635ba2111a
OlegBSTU <polybox45@gmail.com>
2025-01-20 09:17:56 +0500

    Fix code for no hand penalty on painless

--- Neurotrauma/Lua/Scripts/Server/characterpatches.lua
+++ Neurotrauma/Lua/Scripts/Server/characterpatches.lua
@@ -1,15 +1,15 @@
 -- Hooks CalculateMovementPenalty method of Barotrauma.Character
 -- when painless enough, disable weapon sway / movement hindrance limb penalties
 
-local ignoredLimbs = {
-	LimbType.LeftFoot,
-	LimbType.RightFoot,
-	LimbType.LeftHand,
-	LimbType.RightHand,
-}
-
 Hook.Patch("Barotrauma.Character", "CalculateMovementPenalty", function(instance, ptable)
-	if HF.TableContains(ignoredLimbs, ptable["limb"].type) and HF.HasAffliction(instance, "analgesia", 20) then
+	if HF.HasAffliction(instance, "analgesia", 20) then
+		ptable.PreventExecution = true
+		return 0
+	end
+end, Hook.HookMethodType.Before)
+
+Hook.Patch("Barotrauma.AnimController", "GetAimWobble", function(instance, ptable)
+	if HF.HasAffliction(instance.Character, "analgesia", 20) then
 		ptable.PreventExecution = true
 		return 0
 	end
