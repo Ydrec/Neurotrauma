94ec4f255072c9ded01720dee323852d3818a1df
OlegBSTU <polybox45@gmail.com>
2025-02-22 14:05:00 +0500

    Unconsciousness hook, fixed uncon related talents

--- Neurotrauma/Lua/Scripts/Server/characterpatches.lua
+++ Neurotrauma/Lua/Scripts/Server/characterpatches.lua
@@ -1,6 +1,7 @@
 -- Hooks CalculateMovementPenalty method of Barotrauma.Character
 -- when painless enough, disable weapon sway / movement hindrance limb penalties
 
+-- Disable movement penalties for painless characters
 Hook.Patch("Barotrauma.Character", "CalculateMovementPenalty", function(instance, ptable)
 	if HF.HasAffliction(instance, "analgesia", 20) then
 		ptable.PreventExecution = true
@@ -8,9 +9,21 @@ Hook.Patch("Barotrauma.Character", "CalculateMovementPenalty", function(instance
 	end
 end, Hook.HookMethodType.Before)
 
+-- Disable aim penalties for painless characters
 Hook.Patch("Barotrauma.AnimController", "GetAimWobble", function(instance, ptable)
 	if HF.HasAffliction(instance.Character, "analgesia", 20) then
 		ptable.PreventExecution = true
 		return 0
 	end
 end, Hook.HookMethodType.Before)
+
+-- Patch to cause unconscious from the game rather than stun
+Hook.Patch("Barotrauma.CharacterHealth", "get_IsUnconscious", function(instance, ptable)
+	local isUnconscious = HF.HasAffliction(instance.Character, "sym_unconsciousness")
+	ptable.PreventExecution = true
+	return instance.Character.IsDead
+		or (
+			(instance.Character.Vitality <= 0.0 or isUnconscious)
+			and not instance.Character.HasAbilityFlag(AbilityFlags.AlwaysStayConscious)
+		)
+end, Hook.HookMethodType.After)
--- Neurotrauma/Lua/Scripts/Server/humanupdate.lua
+++ Neurotrauma/Lua/Scripts/Server/humanupdate.lua
@@ -920,7 +920,7 @@ NT.Afflictions = {
 					NTC.GetSymptom(c.character, i)
 					or c.stats.stasis
 					or c.afflictions.brainremoved.strength > 0
-					or (not HF.HasAffliction(c.character, "implacable", 0.05) and (c.character.Vitality <= 0 or c.afflictions.hypoxemia.strength > 80))
+					or c.afflictions.hypoxemia.strength > 80
 					or c.afflictions.cerebralhypoxia.strength > 100
 					or c.afflictions.coma.strength > 15
 					or c.afflictions.t_arterialcut.strength > 0
@@ -928,9 +928,9 @@ NT.Afflictions = {
 					or c.afflictions.opiateoverdose.strength > 60
 				)
 			c.afflictions[i].strength = HF.BoolToNum(isUnconscious, 2)
-			if isUnconscious then
-				c.afflictions.stun.strength = math.max(7, c.afflictions.stun.strength)
-			end
+			--if isUnconscious then
+			--	c.afflictions.stun.strength = math.max(7, c.afflictions.stun.strength)
+			--end
 		end,
 	},
 	tachycardia = {
