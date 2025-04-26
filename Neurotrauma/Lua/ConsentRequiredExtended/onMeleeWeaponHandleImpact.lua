2f4ae16b07f375dd235f741c66707966dc32c130
OlegBSTU <78216412+OlegBSTU@users.noreply.github.com>
2025-04-26 23:38:57 +0500

    Consent disabled by default

--- Neurotrauma/Lua/ConsentRequiredExtended/onMeleeWeaponHandleImpact.lua
+++ Neurotrauma/Lua/ConsentRequiredExtended/onMeleeWeaponHandleImpact.lua
@@ -7,7 +7,7 @@ end
 ---@param meleeweapon Weapon target
 ---@param target The target of the hit could be a limb or just a character.
 local function onMeleeWeaponHandleImpact(meleeweapon, target)
-	if not NTConfig.Get("NTCRE_ConsentRequired", true) then
+	if not NTConfig.Get("NTCRE_ConsentRequiredExtra", true) then
 		return
 	end
 	if meleeweapon == nil or target == nil then
